72506fe9027827b291ab7ab1f78d24e8e6df2f49
史少飞 <shishaofei@moqipobing.com>
2022-07-05 14:57:57 +0800

    feat: init

---
+++ packages/api-common/index.js
@@ -0,0 +1,11 @@
+export const add = (a, b) => {
+  return a + b
+}
+
+export const reduce = () => {
+  return a - b
+}
+
+export const ride = () => {
+  return a * b
+}
---
+++ packages/biz-demo/index.js
@@ -0,0 +1,5 @@
+import {
+  add
+} from '@nx-test/api-common'
+
+console.log(add(1, 2))
